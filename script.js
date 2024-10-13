1aa916171e39d616617ee0f5f57b87fadd578660
RaeesaSirkhot <raeesasirkhot@gmail.com>
2024-10-13 05:56:13 +0200

    Update js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=261aoadf0f38dt20ba85250b94b30d7a&units=metric";
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
